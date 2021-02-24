14d90696d9bd7752f35de385280d25e9b6a784ba
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-24 16:07:25 +0000

    space added

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+"use strict"
 document.getElementById('click').addEventListener('click', multiply)
 
 let firstInt = 0
 let secondInt = 0
 let product = 0
 
-function multiply() {
+function multiply () {
   firstInt = document.getElementById('first').value
   firstInt = parseInt(firstInt)
   secondInt = document.getElementById('second').value
   for (let i = 0; i < secondInt; i++) {
-    product = product + firstInt
-  } 
+  product = product + firstInt
+  }
   document.getElementById('result').innerHTML = 'Your product is ' + product
 }
