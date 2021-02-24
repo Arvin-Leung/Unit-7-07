75be2fcb9307dbf1603092a576d9bdee0f91e091
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-24 15:57:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('click').addEventListener('click', multiply)
+
+let firstInt = 0
+let secondInt = 0
+let product = 0
+
+function multiply() {
+  firstInt = document.getElementById('first').value
+  firstInt = parseInt(firstInt)
+  secondInt = document.getElementById('second').value
+  for (let i = 0; i < secondInt; i++) {
+    product = product + firstInt
+  } 
+  document.getElementById('result').innerHTML = 'Your product is ' + product
+}
